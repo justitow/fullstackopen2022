07fe8c9eb49362678a1b4fe4b138ca0a5120dc67
justitow <justitow@gmail.com>
2022-07-06 10:10:43 -0700

    completed 2a

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,14 @@
+const Course = ({course}) => {
+
+    const total = course.parts.reduce((p, a) => p + a.exercises, 0)
+  
+    return (
+      <div>
+        <h1>{course.name}</h1>
+        {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+        <p><b>total of {total} exercises</b></p>
+      </div>
+    )
+  }
+
+  export default Course
