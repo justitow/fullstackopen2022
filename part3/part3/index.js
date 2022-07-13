04be0507517efc39e49618fe0fe29e695b70264c
justitow <justitow@gmail.com>
2022-07-13 13:03:26 -0700

    heroku app creation

--- part3/part3/index.js
+++ part3/part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const nodemon = require('nodemon')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -96,6 +98,7 @@ const unkownEndpoint = (request, response) => {
 
 app.use(unkownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
