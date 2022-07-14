1da68ea28b17683b6cf597b3f02d1f48cbd3fdba
justitow <justitow@gmail.com>
2022-07-14 10:46:27 -0700

    Completion of excercises 3.15 to 3.18

--- part3/excercises/index.js
+++ part3/excercises/index.js
@@ -1,12 +1,18 @@
+require('dotenv').config()
+
+
 const express  = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
+
 
 app.use(cors())
-app.use(express.json())
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('build'))
+app.use(express.json())
 
 morgan.token('body', function(req, res) {
     const body = req.body
@@ -17,57 +23,36 @@ morgan.token('body', function(req, res) {
 })
 
 
-let persons = 
-[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  }) 
 
 app.get('/api/info', (request, response) => {
-    const time = new Date()
-    const count = persons.length
-    response.send(`<p>Phonebook has info for ${count} people</p><p>${time}</p>`)
-})
+    Person.find({}).then(persons => {
+        const time = new Date()
+        const count = persons.length
+        response.send(`<p>Phonebook has info for ${count} people</p><p>${time}</p>`)
+      })
+    }) 
+    
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id ===id)
-    
-    if (person) {
-        response.json(person)
-    }
-    else {
-        response.status(404).end()
-    }
-})
+    Person.findById(request.params.id).then( person => { if (person) {
+        response.json(person)  } else {
+            response.status(404).end()
+        }})
+        .catch(error => next(error))
+        })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter( person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateID = () => {
@@ -76,35 +61,49 @@ const generateID = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    if (!body.name) {
-        return response.status(400).json({
-            error:'name missing'
-        })
+  
+    if (body.name === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
+  
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+      date: new Date(),
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
 
-    if (!body.number) {
-        return response.status(400).json({
-            error:'number missing'
-        })
-    }
-
-    if (persons.some(person => person.name === body.name)){
-        return response.status(400).json({
-            error:'name must be unique'
-        })
-    }
-
-
-    const person = {
-        id: generateID(),
-        name: body.name,
-        number: body.number
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- part3/part3/index.js
+++ part3/part3/index.js
@@ -1,8 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
+
 const app = express()
+
+
 app.use(express.json())
 app.use(cors())
+
+
+
+
+
+const PORT = process.env.PORT
+
+
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -42,19 +56,16 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
+  }) 
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    }
-    else {
-        response.status(404).end()
-    }
-})
+  app.get('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+  })
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -64,24 +75,23 @@ const generateId = () => {
 }
 
 
-app.post('/api/notes', (request, response) =>  {
+app.post('/api/notes', (request, response) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({error: 'content missing'})
-
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
-    
-    notes = notes.concat(note)
-    response.json(note)
-})
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -93,7 +103,6 @@ app.delete('/api/notes/:id', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 3001
 app.set("port", PORT)
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
